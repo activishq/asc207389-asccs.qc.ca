5bff89aeb1ce35b4de909ea73744010fef82beea
ptardif <ptardif@activis.ca>
2019-06-04 16:16:58 -0400

    Test du footer

--- footer.php
+++ footer.php
@@ -18,4 +18,42 @@ if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_
 <?php wp_footer(); ?>
 
 </body>
+<?php 
+	// Bloc pour les données structurées
+	$author = get_the_author(); 
+	$featured_img_url = get_the_post_thumbnail_url($post->ID, 'full'); 
+	$date_modified = date("Y-m-d", get_post_modified_time());
+	$extrait = get_field("extrait");
+	$lien_page = get_permalink();
+	$main_logo = get_home_url()."/wp-content/uploads/2019/06/structured_data_reduce_logo.png";
+?> 
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "NewsArticle",
+  "mainEntityOfPage": {
+         "@type": "WebPage",
+         "@id": "<?php echo $lien_page; ?>"
+  },
+  "headline": "<?php echo get_the_title(); ?>",
+  "image": [
+    "<?php echo $featured_img_url; ?>"
+   ],
+  "datePublished": "<?php echo get_the_date(); ?>",
+  "dateModified": "<?php echo $date_modified; ?>",
+  "author": {
+    "@type": "Person",
+    "name": "<?php echo $author ?>"
+  },
+   "publisher": {
+    "@type": "Organization",
+    "name": "Association sportive et communautaire du Centre Sud",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "<?php echo $main_logo; ?>"
+    }
+  },
+  "description": "<?php echo $extrait; ?>"
+}
+</script>
 </html>
