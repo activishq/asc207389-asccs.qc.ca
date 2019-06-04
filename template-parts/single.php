38975d8a0e8ab5fe1fedbafca53b5406e1203b7b
ptardif <ptardif@activis.ca>
2019-06-04 15:35:21 -0400

    Test pour les données structurées

--- template-parts/single.php
+++ template-parts/single.php
@@ -12,10 +12,37 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <main id="main" <?php post_class( 'site-main' ); ?> role="main">
 
+<?php 
+	$author = get_the_author(); 
+	$featured_img_url = get_the_post_thumbnail_url($post->ID, 'full'); 
+	$extrait = get_field("extrait");
+?> 
+
 	<div class="page-content">
 		<?php the_content(); ?>
 	</div>
 
 </main>
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "NewsArticle",
+  "headline": "<?php echo get_the_title(); ?>",
+  "image": [
+    "<?php echo $featured_img_url; ?>"
+   ],
+  "datePublished": "<?php echo get_the_date(); ?>",
+  "dateModified": "<?php echo get_post_modified_time() ?>",
+  "author": {
+    "@type": "Person",
+    "name": "<?php echo $author ?>"
+  },
+  "description": "<?php echo $extrait ?>"
+}
+</script>
 
-<?php endwhile;
+
+
+<?php endwhile; ?>
+
+
